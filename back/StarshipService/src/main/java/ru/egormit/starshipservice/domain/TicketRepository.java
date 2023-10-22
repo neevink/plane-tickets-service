ac032406a1aae9000ad49fa29a729c7d8519ea64
Kirill Neevin <neevin@yandex-team.ru>
2023-10-22 15:06:08 +0300

    fix

--- back/StarshipService/src/main/java/ru/egormit/starshipservice/domain/TicketRepository.java
+++ back/StarshipService/src/main/java/ru/egormit/starshipservice/domain/TicketRepository.java
@@ -1,6 +1,7 @@
 package ru.egormit.starshipservice.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import ru.itmo.library.Ticket;
 
 /**
@@ -8,5 +9,5 @@ import ru.itmo.library.Ticket;
  *
  * @author Egor Mitrofanov.
  */
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
+public interface TicketRepository extends JpaRepository<Ticket, Long>, JpaSpecificationExecutor<Ticket> {
 }
